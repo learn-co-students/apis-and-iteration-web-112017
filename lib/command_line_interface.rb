da82f17c1672580f06e704bbaa0b447e0b70ce00
mattfaircloth <mattcfaircloth@gmail.com>
2017-11-21 14:20:24 -0500

    Upadted Files to Print List

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,10 @@
 def welcome
-  # puts out a welcome message here!
+  puts "Welcome to the Star Wars Character Info App"
 end
 
 def get_character_from_user
   puts "please enter a character"
   # use gets to capture the user's input. This method should return that input, downcased.
+  user_input = gets.chomp.downcase
+  #return user_input.downcase
 end
