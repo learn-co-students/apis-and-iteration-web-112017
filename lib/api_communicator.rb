a00a6a02bcd52f960b44071fae2b3bb027240df3
github username <siobhan.p.mahoney@gmail.com>
2017-11-21 14:24:25 -0500

    commented out bonus

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,10 +2,10 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
-def formatted_api_hashes(result)
-
-all_characters = RestClient.get('http://www.swapi.co/api/people/')
-character_hash = JSON.parse(all_characters)
+# def formatted_api_hashes(result)
+#
+# all_characters = RestClient.get('http://www.swapi.co/api/people/')
+# character_hash = JSON.parse(all_characters)
 
 def get_character_movies_from_api(character)
   #make the web request
