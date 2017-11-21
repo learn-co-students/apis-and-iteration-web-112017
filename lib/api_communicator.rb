1c38075640f1304f668513237ec68adf055e1179
github username <github email address>
2017-11-21 11:56:48 -0500

    everything but bonus is done

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -6,11 +6,22 @@ def get_character_movies_from_api(character)
   #make the web request
   all_characters = RestClient.get('http://www.swapi.co/api/people/')
   character_hash = JSON.parse(all_characters)
-  
+
+
   # iterate over the character hash to find the collection of `films` for the given
   #   `character`
+  character_hash["results"].each do |char|
+      if char["name"].downcase == character
+         return char["films"].collect do |film_url|
+          string_of_hashes = RestClient.get(film_url)
+          JSON.parse(string_of_hashes)
+        end
+      end
+    end
+
   # collect those film API urls, make a web request to each URL to get the info
   #  for that film
+
   # return value of this method should be collection of info about each film.
   #  i.e. an array of hashes in which each hash reps a given film
   # this collection will be the argument given to `parse_character_movies`
@@ -19,14 +30,17 @@ def get_character_movies_from_api(character)
 end
 
 def parse_character_movies(films_hash)
+  films_hash.each_with_index do |film, index|
+    puts "#{index+1}. #{film["title"]}"
+  end
   # some iteration magic and puts out the movies in a nice list
 end
 
 def show_character_movies(character)
+  #binding.pry
   films_hash = get_character_movies_from_api(character)
   parse_character_movies(films_hash)
 end
-
 ## BONUS
 
 # that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
